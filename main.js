49effa45a1a5e3f385cd9f47bf344391c2ed82ce
KuhuSN <v.nancyadav15@gmail.com>
2022-10-19 00:56:41 +0530

    QuerySelector Commit

--- main.js
+++ main.js
@@ -49,22 +49,43 @@
 
 // GetElementsByTagName
 
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-
-li[1].textContent = 'Hello 2';
-li[1].style.fontWeight = 'bold';
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
 //li[1].style.background = 'yellow';
 
 // Gives Error
 
 //li.style.backgroundColor = '#ffff00';
 
-for(var i=0;i<li.length;i++)
-{
-    li[i].style.backgroundColor = '#ffff00';
-}
+// for(var i=0;i<li.length;i++)
+// {
+//     li[i].style.backgroundColor = '#ffff00';
+// }
 
 
+// QuerySelector
 
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = 'Hello World!';
+
+var submit = document.querySelector('input[type = "submit"]');
+submit.value = "SEND";
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'coral';
+secondItem.style.backgroundColor = 'green';
+
+var ThirdItem = document.querySelector('.list-group-item:nth-child(3)');
+ThirdItem.style.visibility = 'hidden';
