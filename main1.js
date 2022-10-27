eb0860d24c1adad557c927daa8530f885323e9f5
KuhuSN <v.nancyadav15@gmail.com>
2022-10-28 01:04:55 +0530

    Multiple user store

--- main1.js
+++ main1.js
@@ -27,43 +27,39 @@ my_form.addEventListener('submit',storeValue);
 
 function storeValue(e){
     e.preventDefault();
-    debugger;
-    var user_details =  {
+    let user_details = JSON.parse(localStorage.getItem('user_details')) || [];
+    user_details.push({
         user_name : document.getElementById('name').value,
         user_email : document.getElementById('email').value
-    }
+        });
 
     if(user_details.user_name != '' && user_details.user_email != ''){
         localStorage.setItem('user_details', JSON.stringify(user_details));
 
-        console.log(localStorage.getItem('user_details'));
+      // console.log(localStorage.getItem('user_details'));
     
     }else{
         alert('Please fill the details');
     }
-    //showDetails();
+    showDetails();
+    
+    
 }
 
-// function showDetails(){
-//     debugger
-//     if(localStorage.getItem('user_details')){
-//           user_details.user_name = JSON.parse(localStorage.getItem('user_details'));
-//           user_details.user_email = JSON.parse(localStorage.getItem('user_details'));
-//           var output = document.getElementById('output');
-//           output.innerHTML = `
-//             <table>
-//                 <tbody>
-//                     <tr>
-//                         <td> Name </td>
-//                         <td>${user_name}</td>
-//                     </tr>
-//                     <tr>
-//                         <td> Email </td>
-//                         <td>${user_email}</td>
-//                     </tr>
-//                 </tbody>
-//             </table>
-//             `          
-//     }
-// }
+function showDetails(){
+    console.log(localStorage.getItem('user_details'));
+    if(localStorage.getItem('user_details')){
+         //let{user_name ,user_email }= JSON.parse(localStorage.getItem('user_details'));
+          var output = document.querySelector('tbody');
+          output.innerHTML = "";
+          JSON.parse(localStorage.getItem('user_details')).forEach(data => {
+          output.innerHTML += `
+                    <tr>
+                        <td>${data.user_name}</td>
+                        <td>${data.user_email}</td>
+                    </tr>
+            `;          
+        });
+    }
+}
 
